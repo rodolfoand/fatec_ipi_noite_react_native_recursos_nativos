8d2ca3ea21f1d51030399c5146229d7041084c7b
rodolfoand <rodolfo.andrade@fatec.sp.gov.br>
2020-07-03 21:12:31 -0300

    primeiro commit

---
+++ telas/DetalhesDoLugarTela.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const DetalhesDoLugarTela = (props) => {
+  return (
+  <View>
+        <Text>DetalhesDoLugarTela</Text>
+    </View>
+    );
+}
+
+const estilos = StyleSheet.create({
+
+});
+
+export default DetalhesDoLugarTela;
---
+++ telas/MapaTela.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const MapaTela = (props) => {
+  return (
+  <View>
+        <Text>MapaTela</Text>
+    </View>
+    );
+}
+
+const estilos = StyleSheet.create({
+
+});
+
+export default MapaTela;
